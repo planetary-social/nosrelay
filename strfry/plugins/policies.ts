5153a01fa15428269ced7c40adb6ced504e40eeb
mplorentz <mplorentz@users.noreply.github.com>
2025-01-03 16:17:16 -0500

    Remove more broadcastVanish policy code

--- strfry/plugins/policies.ts
+++ strfry/plugins/policies.ts
@@ -9,7 +9,6 @@ import {
   writeStdout,
 } from "https://raw.githubusercontent.com/planetary-social/strfry-policies/refs/heads/nos-changes/mod.ts";
 import nosPolicy from "./nos_policy.ts";
-import { createBroadcastVanishRequests } from "./broadcast_vanish_requests.ts";
 import { connect, parseURL } from "https://deno.land/x/redis/mod.ts";
 
 const localhost = "127.0.0.1";
@@ -18,7 +17,6 @@ const redis_connect_options = parseURL(redis_url);
 const redis = await connect(redis_connect_options);
 
 const relay_url = Deno.env.get("RELAY_URL");
-const broadcastVanishRequests = createBroadcastVanishRequests(redis, relay_url);
 
 // Policies that reject faster should be at the top. So synchronous policies should be at the top.
 const policies = [
