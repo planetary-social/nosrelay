b0117dd5bf5f0ed61a595a22fb7dd0950356430d
mplorentz <mplorentz@users.noreply.github.com>
2025-01-09 07:26:29 -0500

    Exempt sync.nos.social server from rate limiting

--- strfry/plugins/policies.ts
+++ strfry/plugins/policies.ts
@@ -11,6 +11,7 @@ import {
 import nosPolicy from "./nos_policy.ts";
 
 const localhost = "127.0.0.1";
+const sync_server = "159.65.45.194"; // sync.nos.social
 
 // Policies that reject faster should be at the top. So synchronous policies should be at the top.
 const policies = [
@@ -18,7 +19,7 @@ const policies = [
   [hellthreadPolicy, { limit: 100 }],
   // Async policies
   [antiDuplicationPolicy, { ttl: 60000, minLength: 50 }],
-  [rateLimitPolicy, { whitelist: [localhost] }],
+  [rateLimitPolicy, { whitelist: [localhost, sync_server] }],
 ];
 
 for await (const msg of readStdin()) {
