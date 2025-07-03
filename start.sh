5e092293fe962bb5d6d2be8efe7560e58c92f14b
Daniel Cadenas <dcadenas@gmail.com>
2025-07-03 13:27:48 -0300

    Prepare for negentropy syncing

--- start.sh
+++ start.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# Start the relay in the background
 /app/strfry relay &
+RELAY_PID=$!
+
+# Start vanish subscriber
 /app/vanish_subscriber &
+VANISH_PID=$!
+
+# Only start router if config exists and relay_sync_peers is configured
+if [ -f "/etc/strfry-router.conf" ]; then
+    echo "Starting strfry router..."
+    /app/strfry router /etc/strfry-router.conf &
+    ROUTER_PID=$!
+fi
 
 # Wait for any process to exit
-wait -n
+wait -n $RELAY_PID $VANISH_PID ${ROUTER_PID:-}
 
 EXIT_STATUS=$?
-pkill -P $$
+
+# If one process dies, kill the others
+kill $RELAY_PID $VANISH_PID ${ROUTER_PID:-} 2>/dev/null
 
 # Exit with the status of the first exited process
 exit $EXIT_STATUS
